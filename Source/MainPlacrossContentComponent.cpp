90cb9901f56b0d7211a9cb319c407f4c92925a14
Christian Ahrens <christianahrens@me.com>
2020-06-16 11:44:28 +0200

    Created Projucer Placross project based on PlayingSoundFilesTutorial

---
+++ Source/MainPlacrossContentComponent.cpp
@@ -0,0 +1,214 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainPlacrossContentComponent.h"
+
+//==============================================================================
+    MainPlacrossContentComponent::MainPlacrossContentComponent()
+        :   state (Stopped)
+    {
+        addAndMakeVisible (&openButton);
+        openButton.setButtonText ("Open...");
+        openButton.onClick = [this] { openButtonClicked(); };
+
+        addAndMakeVisible (&playButton);
+        playButton.setButtonText ("Play");
+        playButton.onClick = [this] { playButtonClicked(); };
+        playButton.setColour (TextButton::buttonColourId, Colours::green);
+        playButton.setEnabled (false);
+
+        addAndMakeVisible (&stopButton);
+        stopButton.setButtonText ("Stop");
+        stopButton.onClick = [this] { stopButtonClicked(); };
+        stopButton.setColour (TextButton::buttonColourId, Colours::red);
+        stopButton.setEnabled (false);
+
+        addAndMakeVisible (&loopingToggle);
+        loopingToggle.setButtonText ("Loop");
+        loopingToggle.onClick = [this] { loopButtonChanged(); };
+
+        addAndMakeVisible (&currentPositionLabel);
+        currentPositionLabel.setText ("Stopped", dontSendNotification);
+
+        setSize (300, 200);
+
+        formatManager.registerBasicFormats();
+        transportSource.addChangeListener (this);
+        
+        // Some platforms require permissions to open input channels so request that here
+        if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
+            && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+        {
+            RuntimePermissions::request (RuntimePermissions::recordAudio,
+                                        [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+        }
+        else
+        {
+            // Specify the number of input and output channels that we want to open
+            setAudioChannels (2, 2);
+        }
+        startTimer (20);
+    }
+
+    MainPlacrossContentComponent::~MainPlacrossContentComponent()
+    {
+        // This shuts down the audio device and clears the audio source.
+        shutdownAudio();
+    }
+
+    void MainPlacrossContentComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+    {
+        // This function will be called when the audio device is started, or when
+        // its settings (i.e. sample rate, block size, etc) are changed.
+
+        // You can use this function to initialise any resources you might need,
+        // but be careful - it will be called on the audio thread, not the GUI thread.
+
+        // For more details, see the help for AudioProcessor::prepareToPlay()
+        transportSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
+    }
+
+    void MainPlacrossContentComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+    {
+        // Your audio-processing code goes here!
+
+        // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+        // Right now we are not producing any data, in which case we need to clear the buffer
+        // (to prevent the output of random noise)
+        if (readerSource.get() == nullptr)
+        {
+            bufferToFill.clearActiveBufferRegion();
+            return;
+        }
+
+        transportSource.getNextAudioBlock (bufferToFill);
+    }
+
+    void MainPlacrossContentComponent::releaseResources()
+    {
+        // This will be called when the audio device stops, or when it is being
+        // restarted due to a setting change.
+
+        // For more details, see the help for AudioProcessor::releaseResources()
+        transportSource.releaseResources();
+    }
+
+    void MainPlacrossContentComponent::resized()
+    {
+        openButton          .setBounds (10, 10,  getWidth() - 20, 20);
+        playButton          .setBounds (10, 40,  getWidth() - 20, 20);
+        stopButton          .setBounds (10, 70,  getWidth() - 20, 20);
+        loopingToggle       .setBounds (10, 100, getWidth() - 20, 20);
+        currentPositionLabel.setBounds (10, 130, getWidth() - 20, 20);
+    }
+
+    void MainPlacrossContentComponent::changeListenerCallback (ChangeBroadcaster* source)
+    {
+        if (source == &transportSource)
+        {
+            if (transportSource.isPlaying())
+                changeState (Playing);
+            else
+                changeState (Stopped);
+        }
+    }
+
+    void MainPlacrossContentComponent::timerCallback()
+    {
+        if (transportSource.isPlaying())
+        {
+            RelativeTime position (transportSource.getCurrentPosition());
+
+            auto minutes = ((int) position.inMinutes()) % 60;
+            auto seconds = ((int) position.inSeconds()) % 60;
+            auto millis  = ((int) position.inMilliseconds()) % 1000;
+
+            auto positionString = String::formatted ("%02d:%02d:%03d", minutes, seconds, millis);
+
+            currentPositionLabel.setText (positionString, dontSendNotification);
+        }
+        else
+        {
+            currentPositionLabel.setText ("Stopped", dontSendNotification);
+        }
+    }
+
+    void MainPlacrossContentComponent::updateLoopState (bool shouldLoop)
+    {
+        if (readerSource.get() != nullptr)
+            readerSource->setLooping (shouldLoop);
+    }
+
+    void MainPlacrossContentComponent::changeState (TransportState newState)
+    {
+        if (state != newState)
+        {
+            state = newState;
+
+            switch (state)
+            {
+                case Stopped:
+                    stopButton.setEnabled (false);
+                    playButton.setEnabled (true);
+                    transportSource.setPosition (0.0);
+                    break;
+
+                case Starting:
+                    playButton.setEnabled (false);
+                    transportSource.start();
+                    break;
+
+                case Playing:
+                    stopButton.setEnabled (true);
+                    break;
+
+                case Stopping:
+                    transportSource.stop();
+                    break;
+            }
+        }
+    }
+
+    void MainPlacrossContentComponent::openButtonClicked()
+    {
+        FileChooser chooser ("Select a Wave file to play...",
+                             {},
+                             "*.wav");
+
+        if (chooser.browseForFileToOpen())
+        {
+            auto file = chooser.getResult();
+            auto* reader = formatManager.createReaderFor (file);
+
+            if (reader != nullptr)
+            {
+                std::unique_ptr<AudioFormatReaderSource> newSource (new AudioFormatReaderSource (reader, true));
+                transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);
+                playButton.setEnabled (true);
+                readerSource.reset (newSource.release());
+            }
+        }
+    }
+
+    void MainPlacrossContentComponent::playButtonClicked()
+    {
+        updateLoopState (loopingToggle.getToggleState());
+        changeState (Starting);
+    }
+
+    void MainPlacrossContentComponent::stopButtonClicked()
+    {
+        changeState (Stopping);
+    }
+
+    void MainPlacrossContentComponent::loopButtonChanged()
+    {
+        updateLoopState (loopingToggle.getToggleState());
+    }
+
---
+++ Source/MainPlacrossContentComponent.h
@@ -0,0 +1,71 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainPlacrossContentComponent   : public AudioAppComponent,
+                               public ChangeListener,
+                               public Timer
+{
+public:
+    MainPlacrossContentComponent();
+    ~MainPlacrossContentComponent() override;
+
+    //==========================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+    
+    //==========================================================================
+    void resized() override;
+    
+    //==========================================================================
+    void changeListenerCallback (ChangeBroadcaster* source) override;
+    
+    //==========================================================================
+    void timerCallback() override;
+    
+    void updateLoopState (bool shouldLoop);
+
+private:
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+
+    void changeState (TransportState newState);
+
+    void openButtonClicked();
+    void playButtonClicked();
+    void stopButtonClicked();
+    void loopButtonChanged();
+
+    //==========================================================================
+    TextButton openButton;
+    TextButton playButton;
+    TextButton stopButton;
+    ToggleButton loopingToggle;
+    Label currentPositionLabel;
+
+    AudioFormatManager formatManager;
+    std::unique_ptr<AudioFormatReaderSource> readerSource;
+    AudioTransportSource transportSource;
+    TransportState state;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainPlacrossContentComponent)
+};
